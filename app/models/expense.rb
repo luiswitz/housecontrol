31dc46bb1980db665675ede856c082d2602775b8
Luis Henrique Witz <luiswitz@gmail.com>
2017-03-11 15:18:53 -0300

    Tests for expense model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,7 @@ class Expense < ApplicationRecord
   belongs_to :credit_card, optional: true
   belongs_to :category
   belongs_to :user
+
+  validates_presence_of :value, :category, :date, :user
+  validates :value, numericality: { greater_than: 0 }
 end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "when create a simple expense" do
+    before do
+      @expense = FactoryGirl.create(:expense)
+    end
+
+    it "cannot be valid without a value" do
+      @expense.value = nil
+      expect(@expense).to_not be_valid
+    end
+
+    it "cannot be valid without a category" do
+      @expense.category = nil
+      expect(@expense).to_not be_valid
+    end
+
+    it "cannot be valid without a date" do
+      @expense.date = nil
+      expect(@expense).to_not be_valid
+    end
+
+    it "cannot be valid with value less or equal 0" do
+      @expense.value = 0.0
+      expect(@expense).to_not be_valid
+    end
+
+    it "cannot be valid without an user" do
+      @expense.user = nil
+      expect(@expense).to_not be_valid
+    end
+  end
 end
