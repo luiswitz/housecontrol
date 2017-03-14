5fcb9ed14841b0848b4a36e34ed76047b902b809
Luis Henrique Witz <luiswitz@gmail.com>
2017-03-14 09:18:38 -0300

    Implemented creation of an expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,4 +1,5 @@
 class ExpensesController < ApplicationController
+  before_action :set_income, only: [:edit]
 
   def index
     @expenses = Expense.all
@@ -7,5 +8,29 @@ class ExpensesController < ApplicationController
   def new
     @expense = Expense.new
   end
+
+  def create
+    @expense = Expense.new(expense_params)
+    @expense.user_id = current_user.id
+    if @expense.save
+      redirect_to expenses_path, notice: 'Sua despesa foi adicionada com sucesso'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+
+    def expense_params
+        params.require(:expense).permit(:description, :value, :date, :category_id, :is_payed)
+    end
+
+    def set_income
+      @expense = Expense.find(params[:id])
+    end
 
 end
--- spec/features/expense_spec.rb
+++ spec/features/expense_spec.rb
@@ -35,4 +35,22 @@ describe "navigate" do
       expect(page.status_code).to eq(200)
     end
   end
+
+  describe "create" do
+    before do
+      visit new_expense_path
+    end
+
+    it "has a form that can be reached" do
+      expect(page.status_code).to eq(200)
+    end
+
+    it "can be created from new form page" do
+      fill_in 'expense[description]', with: 'Some expense'
+      fill_in 'expense[value]', with: 9.99
+      fill_in 'expense[date]', with: Date.today
+      select "#{@category.name}", :from => "expense[category_id]"
+      expect { click_on 'Adicionar'}.to change(Expense, :count).by(1)
+    end
+  end
 end
