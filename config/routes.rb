dbae52a3dab5f991d5af72eee7e858ce415d202a
Luis Henrique Witz <luiswitz@gmail.com>
2017-03-29 18:15:43 -0300

    Changed root route and login path names

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :credit_card_expenses
   resources :incomes
   resources :expenses
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  root to: 'expenses#index'
 end
