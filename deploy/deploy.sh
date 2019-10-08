a3afb6fdc6c3286fe9a6f2d05d264f1890dba65f
Luis Henrique Witz <luiswitz@gmail.com>
2019-10-07 22:29:31 -0300

    Rename docker-compose production and fix improve deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,12 +2,14 @@ DIR="expensescontrol"
 
 if [ -d "$DIR" ]; then
   cd $DIR
-  git pull origin https://github.com/luiswitz/expensescontrol.git
+  git pull origin master
 else
   git clone https://github.com/luiswitz/expensescontrol.git
   cd $DIR
 fi
 
-docker-compose down
-docker-compose pull
-docker-compose up -d
+docker-compose -f docker-compose.production.yml down
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm backend bundle install
+docker-compose -f docker-compose.production.yml run --rm backend bundle exec rails db:create db:migrate
+docker-compose -f docker-compose.production.yml up -d
