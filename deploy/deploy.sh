946ffd89e8943c10879cc6ff21e835d13ad67d53
Luis Henrique Witz <luiswitz@gmail.com>
2019-10-07 22:34:23 -0300

    Fix deploy (#9)
    Add docker-compose production, fix travis file and improve deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,15 @@
-docker-compose -f docker-compose-production.yml down
-docker-compose -f docker-compose-production.yml pull
-docker-compose -f docker-compose-production.yml up -d
+DIR="expensescontrol"
+
+if [ -d "$DIR" ]; then
+  cd $DIR
+  git pull origin master
+else
+  git clone https://github.com/luiswitz/expensescontrol.git
+  cd $DIR
+fi
+
+docker-compose -f docker-compose.production.yml down
+docker-compose -f docker-compose.production.yml build
+docker-compose -f docker-compose.production.yml run --rm backend bundle install
+docker-compose -f docker-compose.production.yml run --rm backend bundle exec rails db:create db:migrate
+docker-compose -f docker-compose.production.yml up -d
