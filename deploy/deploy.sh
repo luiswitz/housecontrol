0bbdab1695a80c8072e75c9216da75ab76643c10
Luis Henrique Witz <luiswitz@gmail.com>
2019-10-07 21:13:34 -0300

    Fix deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,6 @@ git pull origin git@github.com:luiswitz/expensescontrol.git
 
 cd expenses_control
 
-docker-compose -f docker-compose-production.yml down
-docker-compose -f docker-compose-production.yml pull
-docker-compose -f docker-compose-production.yml up -d
+docker-compose down
+docker-compose pull
+docker-compose up -d
